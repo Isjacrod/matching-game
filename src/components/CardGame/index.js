86a2843aefcd264e32ea12a21ac1d7102fd44176
Isjacrod <israeljacksonrodrigues@gmail.com>
2022-02-10 15:19:34 -0300

    :tada: Initial commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame(textContent) {
+    return (`
+        <article class="card-game">
+            <img src="images/alura-pixel.png" alt="Logo da alura" />
+        </article>
+    `)
+}
+
+export default CardGame;
